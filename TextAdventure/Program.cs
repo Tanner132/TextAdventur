1571ce1d5a5a449cc56ae6412b36a0f0825e8785
Tanner Strickland <tannerstrickland2000@yahoo.com>
2019-08-06 09:14:47 -0500

    Added Player movement and a sword attack

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -20,9 +20,13 @@ namespace TextAdventure
             //Strings
             string Char_Name = "Bastard Inc";
 
-            //Draw GUI
-            //Draw Stats
+            
             do{
+                //Count EXP
+
+
+                //Draw GUI
+                //Draw Stats
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine("________________________________________________________________________________");
                 Console.SetCursorPosition(5, 1);
@@ -44,14 +48,82 @@ namespace TextAdventure
                 Console.SetCursorPosition(0, 22);
                 Console.WriteLine("________________________________________________________________________________");
 
+
+                //Player Controls
                 ConsoleKeyInfo KeyInfo;
                 KeyInfo = Console.ReadKey(true);
                 switch (KeyInfo.Key)
                 {
+                    //moving up down left and right
                     case ConsoleKey.RightArrow:
                         Char_Move_Left_Right++;
                         Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down);
-                        Console.WriteLine("X");
+                        Console.Write("X");
+                        Console.SetCursorPosition(Char_Move_Left_Right - 1, Char_Move_Up_Down);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("X");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Char_Move_Left_Right--;
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down);
+                        Console.Write("X");
+                        Console.SetCursorPosition(Char_Move_Left_Right + 1, Char_Move_Up_Down);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("X");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Char_Move_Up_Down--;
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down);
+                        Console.Write("X");
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down + 1);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("X");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Char_Move_Up_Down++;
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down);
+                        Console.Write("X");
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down - 1);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("X");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case ConsoleKey.Spacebar:
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down);
+                        Console.Write("X");
+
+
+                        // Sword Attack 
+                        System.Threading.Thread.Sleep(30);
+                        Console.SetCursorPosition(Char_Move_Left_Right + 1, Char_Move_Up_Down);
+                        Console.Write("-");
+                        Console.SetCursorPosition(Char_Move_Left_Right + 1, Char_Move_Up_Down);
+                        System.Threading.Thread.Sleep(40);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("-");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        System.Threading.Thread.Sleep(30);
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down - 1 );
+                        Console.Write("|");
+                        Console.SetCursorPosition(Char_Move_Left_Right, Char_Move_Up_Down - 1);
+                        System.Threading.Thread.Sleep(40);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("|");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        System.Threading.Thread.Sleep(30);
+                        Console.SetCursorPosition(Char_Move_Left_Right - 1, Char_Move_Up_Down);
+                        Console.Write("-");
+                        Console.SetCursorPosition(Char_Move_Left_Right - 1, Char_Move_Up_Down);
+                        System.Threading.Thread.Sleep(40);
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("-");
+                        Console.ForegroundColor = ConsoleColor.White;
+
                         break;
                 }
             } while (Game_Over == 0);
